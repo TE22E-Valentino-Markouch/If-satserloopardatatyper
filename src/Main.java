3f01024737606acedf9999825f95c4494c98ede5
Valentino Markouch <Valentino.Markouch@elev.ga.ntig.se>
2025-01-13 10:14:28 +0100

    rahhhhh

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,58 @@
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
 
+        storreEn();
+
+        Anvendarnamn();
+
+
         System.out.println("Mata in en valfri sifra:");
-        //string2Int();
         intInput();
+        //string2Int();
     }
+
+
+    private static void storreEn() {
+        if (6 > 3) {
+            System.out.println("Hello World!");
+        }
+    }
+
+    private static void Anvendarnamn() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            // Hämta användarnamn från användaren
+            System.out.print("Enter your username: ");
+            String username = scanner.nextLine();
+
+            // Kontrollera om användarnamnet är "noname"
+            if (username.equals("noname")) {
+                System.out.println("Welcome!");
+                break;
+            }
+
+            else{}
+        }
+    }
+
+
+
+
+
 
     private static void intInput() {
         Scanner in = new Scanner(System.in);
         int num = 0;
-        while(true) {
+        while (true) {
             try {
                 num = in.nextInt();
                 break;
-            }catch(Exception e) {
+            } catch (Exception e) {
                 System.out.println("NaN try again");
                 in.nextLine();
 
@@ -26,8 +63,8 @@ public class Main {
 
     private static void string2Int() {
         Scanner scan = new Scanner(System.in);
-        int num=0;
-        while(true) {
+        int num = 0;
+        while (true) {
             String myst = scan.nextLine();
 
             try {
