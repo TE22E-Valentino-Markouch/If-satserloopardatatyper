9e75295f19f911ce8deabc09097e1d048ae10de5
Valentino Markouch <Valentino.Markouch@elev.ga.ntig.se>
2025-01-14 19:15:44 +0100

    Färdig

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,33 @@ import java.util.Scanner;
 
 public class Main {
 
-
+    //  Main
     public static void main(String[] args) {
+
 
         storreEn();
 
         Anvendarnamn();
 
-
         System.out.println("Mata in en valfri sifra:");
         intInput();
         //string2Int();
+
+        HelloWorld();
+
+        HögreÄn();
+
+        GissaSiffran();
     }
 
-
+    //  Basic större än kod
     private static void storreEn() {
         if (6 > 3) {
             System.out.println("Hello World!");
         }
     }
 
+    //  Användarnamn
     private static void Anvendarnamn() {
         Scanner scanner = new Scanner(System.in);
 
@@ -30,21 +37,23 @@ public class Main {
             System.out.print("Enter your username: ");
             String username = scanner.nextLine();
 
+            // Hämta lössenord från användaren
+            System.out.println("Enter your password: ");
+            String password = scanner.nextLine();
+
+
             // Kontrollera om användarnamnet är "noname"
-            if (username.equals("noname")) {
+            if (username.equals("noname") && password.equals("nopass")) {
                 System.out.println("Welcome!");
                 break;
+            } else {
+                System.out.println("Wrong username or password!");
             }
-
-            else{}
         }
     }
 
 
-
-
-
-
+    //  Basic Input line 1
     private static void intInput() {
         Scanner in = new Scanner(System.in);
         int num = 0;
@@ -61,6 +70,7 @@ public class Main {
 
     }
 
+    //  Basic Input line 2
     private static void string2Int() {
         Scanner scan = new Scanner(System.in);
         int num = 0;
@@ -75,6 +85,56 @@ public class Main {
             }
 
         }
+    }
+
+    private static void HelloWorld() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello World!");
+        }
+
+    }
+
+    private static void HögreÄn() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Kör loopen 5 gånger
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Skriv in ett tal: ");
+            int tal = scanner.nextInt(); // Läser in ett heltal från användaren
+
+            // Kontrollera om talet är högre än 5
+            if (tal > 5) {
+                System.out.println("Högre än 5!");
+            } else {
+                System.out.println("Mindre än 5!");
+            }
+        }
+    }
+
+    private static void GissaSiffran() {
+        int randomNum = (int) (Math.random() * 101); // 0 to 100
+        System.out.println("Gissa på en siffra mellan 0 till 100:");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            int tal = scanner.nextInt(); // Användarens val
+            if (randomNum == tal) {
+                System.out.println("Rätt");
+                break;
+            } else if (randomNum > tal) {
+                System.out.println("Fel!");
+                System.out.println("Siffran du letar efter är större");
+            } else if (randomNum < tal) {
+                System.out.println("Fel!");
+                System.out.println("Siffran du letar efter är mindre");
+            } else {
+                System.out.println("Var vänligen och ange ett nummer");
+            }
+
+
+        }
+
+
     }
 
 
